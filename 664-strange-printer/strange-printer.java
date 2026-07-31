dc02388ef9d542b31eb4baa214f7dc968e61dda1
Gunasakthi <gunasakthi0803@gmail.com>
2026-07-31 13:55:38 +0530

    Time: 15 ms (76.07%) | Memory: 44.4 MB (91.88%) - LeetSync

---
+++ 664-strange-printer/strange-printer.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int strangePrinter(String s) {
+        final int inf = 1 << 30;
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for (var g : dp) {
+            Arrays.fill(g, inf);
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i][j - 1];
+                } else {
+                    for (int k = i; k < j; k++) {
+                        dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k + 1][j]);
+                    }
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+}
